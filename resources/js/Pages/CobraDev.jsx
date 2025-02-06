f70e3196a707d255643172ec866fd11cda4c16a4
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-02-06 13:06:54 +0700

    first commit

---
+++ resources/js/Pages/CobraDev.jsx
@@ -0,0 +1,13 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function CobraDev() {
+    return (
+        <>
+            <Head title="CobraDev"/>
+            <div className="py-12 text-center bg-white sm:rounded-lg">
+                <h1 className="text-4xl font-bold text-gray-900">CobraDev</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt esse harum repudiandae optio cupiditate aperiam aliquid tempora ex nulla mollitia, iste eaque velit fugiat quam delectus. In ratione quasi accusamus!</p>
+            </div>
+        </>
+    );
+}
