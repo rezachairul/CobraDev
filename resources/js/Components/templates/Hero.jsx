ccedba688b2fae00cfa2d88f51b23e9166c93897
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-02-09 00:01:18 +0700

    Update Section Hero -> social media

--- resources/js/Components/templates/Hero.jsx
+++ resources/js/Components/templates/Hero.jsx
@@ -1,7 +1,15 @@
 "use client";
 import React from "react";
 import {TypewriterEffectSmooth } from "../ui/typewriter-effect";
-import { TextRevealCard, TextRevealCardDescription, TextRevealCardTitle, } from "../ui/text-reveal-card";
+import { TextRevealCard} from "../ui/text-reveal-card";
+import { FloatingDock } from "../ui/floating-dock";
+import {
+    IconMail,
+    IconBrandInstagram,
+    IconBrandLinkedin,
+    IconBrandGithub,
+    IconFileCv,
+} from "@tabler/icons-react";
 
 export default function Hero() {
     const words = [
@@ -13,7 +21,29 @@ export default function Hero() {
         text: "Web Applications.",
         className: "text-purple-500 dark:text-purple-500",
         },
-      ];
+    ];
+    const links = [
+        { title: "Email", icon: (
+            <IconMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ), href: "mailto:rezachairul6@gmail.com",
+        },
+        { title: "Instagram", icon: (
+            <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ), href: "https://www.instagram.com/rezachairul6/",
+        },
+        { title: "LinkedIn", icon: (
+            <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ), href: "linkedin.com/in/reza-chairul6",
+        },
+        { title: "GitHub", icon: (
+            <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ), href: "https://github.com/rezachairul",
+        },
+        { title: "Resume", icon: (
+            <IconFileCv className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ), href: "#",
+        },
+    ];
     return (
         <section className="h-screen flex flex-col justify-center items-center text-center bg-black text-white px-4">
             <img src="/assets/logo.svg" className="mx-auto w-20" />
@@ -22,7 +52,8 @@ export default function Hero() {
                 revealText="Hi, I'm CobraDev"
                 className="font-sans" >
             </TextRevealCard>            
-            <TypewriterEffectSmooth words={words} className="font-sans" />
+            <TypewriterEffectSmooth words={words} className="font-sans mb-16" />
+            <FloatingDock mobileClassName="translate-y-20" items={links} />
         </section>
     );
 }
